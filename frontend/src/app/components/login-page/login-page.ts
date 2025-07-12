c7da827849754066cc2a51590a921bad430ca603
Diego Espindola <diegoespindola9404@gmail.com>
2025-07-11 22:05:11 -0300

    feat(login): implementa formulário reativo e lógica de autenticação com HTTP POST

--- frontend/src/app/components/login-page/login-page.ts
+++ frontend/src/app/components/login-page/login-page.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { enviroment } from '../../../enviroments/enviroment';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone:true,
+  imports: [FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css'
 })
 export class LoginPage {
+  username = '';
+  password = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login() {
+    const body = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.http.post<any>(`${enviroment.apiUrlLogin}/login`, body).subscribe({
+      next: (response) => {
+        console.log('Login OK:', response);
+        // Salvar o user no localStorage ou service
+        localStorage.setItem('user', JSON.stringify(response.user));
+        this.router.navigate(['/todoApp']);
+      },
+      error: (err) => {
+        console.error('Erro ao fazer login:', err);
+        if (err.status === 401) {
+          alert('Credenciais inválidas');
+        } else {
+          alert('Erro no servidor, verifique se seu banco de dados está ativo.');
+        }
+      }
+    });
+  }
 }
