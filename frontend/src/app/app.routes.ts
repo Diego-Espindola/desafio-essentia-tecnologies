70c85b325eb7d4490832fdd3b6799463aac63700
Diego Espindola <diegoespindola9404@gmail.com>
2025-07-11 22:06:46 -0300

    feat(todo-page): cria componente principal para exibir tarefas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './components/login-page/login-page';
+import { TodoPage } from './components/todo-page/todo-page';
 
 export const routes: Routes = [
     {
         path: "",
         component: LoginPage
+    },
+    {
+        path: 'todoApp',
+        component: TodoPage
     }
 ];
