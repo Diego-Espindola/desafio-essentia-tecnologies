fb99f746ce8299e95f8c1d2a073de97a52450f4f
Diego Espindola <diegoespindola9404@gmail.com>
2025-07-14 22:15:01 -0300

    fix: corrigir o aviso KeepAlive

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors  } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors  } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { tokenInterceptor } from './interceptors/token.interceptor';
@@ -10,7 +10,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor]))
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([tokenInterceptor]))
 
   ]
 };
