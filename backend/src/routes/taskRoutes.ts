5e6d25305face385d2c12b6a3cb3554fec19d1c0
Diego Espindola <diegoespindola9404@gmail.com>
2025-07-14 22:17:00 -0300

    feat: adicionar edição de tasks && alterar rota para mudança de status 'markAsDone'

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -8,7 +8,8 @@ router.use(authenticateToken);
 
 router.get('/tasks', taskController.getTasksByUser);
 router.post('/tasks', taskController.createTask);
-router.put('/tasks/:id', taskController.updateTaskStatus);
+router.put('/tasks/status/:id', taskController.updateTaskStatus);
+router.put('/tasks/:id', taskController.editTask);
 router.delete('/tasks/:id', taskController.deleteTask);
 
 export default router;
